c254a393512312936bcf14971c9586ffc27fad1c
Jorge <georgiearclopez@gmail.com>
2024-11-15 12:10:53 +0100

    Adivinar palabra corregido

--- Adivinar Palabra/index.js
+++ Adivinar Palabra/index.js
@@ -55,7 +55,7 @@ window.onload = function() {
         let palabraAModificar = palabra.split("");
         for(let i = 0; i < letrasQuitar; i++){
             let posicionAleatoria = Math.floor(Math.random() * palabraAModificar.length);
-            while(palabraAModificar[posicionAleatoria] == " _ "){
+            while(palabraAModificar[posicionAleatoria] === " _ "){
                 posicionAleatoria = Math.floor(Math.random() * palabraAModificar.length);
             }
             
